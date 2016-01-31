9ec5c12bdea7e4ff3257a955e3e5879e7f131e70
maaario <mlipovskysk@gmail.com>
2016-01-31 21:25:39 +0100

    css and login functionality

--- testovac/urls.py
+++ testovac/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 import testovac.tasks.urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tasks/', include(testovac.tasks.urls)),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
 ]
